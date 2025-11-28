4fd091f9d43d68ba1cbd2d79db069ae4e62df411
julien <forevergoodforest@outlook.com>
2025-11-28 20:02:17 +0800

    chore: release v1.5.3

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="leetcode-fsrs-cli",
-    version="1.5.2",
+    version="1.5.3",
     author="SaintFore",
     author_email="saintfore@example.com",
     description="A CLI tool for LeetCode practice using FSRS spaced repetition algorithm",
